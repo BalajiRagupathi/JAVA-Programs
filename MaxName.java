2e2e0abdf8297bd6a08dc08f22b453c78ca01e7f
BalajiRagupathi <43380898+BalajiRagupathi@users.noreply.github.com>
2019-12-11 18:56:46 +0530

    Update MaxName.java

--- MaxName.java
+++ MaxName.java
@@ -1,21 +1,15 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.*;
 
 public class Main
 {
-    public static HashMap<Long,String> hm = new HashMap<>();
-    
 	public static void main(String[] args) {
+		
+		HashMap<String,Integer> hm = new HashMap<>();
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int max = 0 , ml = 2147483647;
+		String min = "";
 		do{
 		    
 		    System.out.println("Enter name");
@@ -24,20 +18,62 @@ public class Main
 		    System.out.println("Enter No to close");
 		    String countinue = sc.next();
 		    
-		    if( hm.containsValue(name) ){
+		    if( hm.containsKey(name) ){
 		        
-		        int temp = hm.getKey();
+		        int temp = hm.getValue();
 		        temp = temp + 1;
-		        while( hm.containsKey(temp) ){
-		            
-		            temp = temp * 10;
+		        if( temp > max ){
+    		          
+    		          max = temp;
+    		          min = name;
+    		          ml = min.length();
 		        }
-		        hm.put(temp,name);
+		        else if( temp == max ){
+		            
+		            if( ml > name.length() ){  
+        		            
+        		            max = temp;
+        		            min = name;
+        		            ml = min.length();
+    		          }
+		        }
+		        else{ 
+		            
+		            max = temp;
+		            min = name;
+		            ml = min.length();
+		        }
+		        hm.put(name,temp);
 		    }
 		    else{
 		        
-		        hm.put(1,name);
+		        int temp1 = 1;
+		        
+		        if( temp1 > max ){
+    		          
+    		          max = temp1;
+    		          min = name;
+    		          ml = min.length();
+		        }
+		        else if( temp == max ){
+		            
+		            if( min.length() > name.length() ){  
+        		            
+        		            max = temp1;
+        		            min = name;
+        		            ml = min.length();
+    		          }
+		        }
+		        else{ 
+		            
+		            max = temp1;
+		            min = name;
+		            ml = min.length();
+		        }
+		        hm.put(name,temp1);
 		    }
 		}while( !countinue.equals("No") || !countinue.equals("no") );
+		
+		System.out.println("Name = "+min+" Length "+ml+" Count "+max );
 	}
 }
